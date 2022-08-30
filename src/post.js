6afaf0c2181637966f7e93d8ba019f8685ede0b0
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2022-08-30 13:40:52 +0200

    Uploadig the post code

--- src/post.js
+++ src/post.js
@@ -1,38 +1,79 @@
 const postTodo =  async () => 
 {
     try
-    {   let taskToAdd = document.getElementById("userInput").value;
+    {   
+        const taskToAdd = document.getElementById("userInput").value;
+        const listContainer = document.getElementById('myList');
+        const newList = document.createElement('ul');
+        const newListItem = document.createElement('li');
         const res = await fetch('http://localhost:5000/todos',
+        
+        
         {
             method: 'POST',
-            headers: 
-            {
+            headers: {
                 'Content-type': 'application/json',
             },
             body : JSON.stringify({title : taskToAdd, completed : false})
-        });
+            
+        }
+        );
+        
     
         // Checking response
         if (res.status === 201)
         {
             const data = await res.json();
-                const listContainer = document.getElementById('myList');
-                const newList = document.createElement('ul');
-                const newListItem = document.createElement('li');
-                newListItem.innerHTML = data.title;
-                newList.appendChild(newListItem);
-                listContainer.appendChild(newList);
+        // Displaying the data in the browser
+            newListItem.innerHTML = data.title;
+            newList.appendChild(newListItem);
+            listContainer.appendChild(newList);  
+
+        // Creating a delete button
+       const myNodelist = newListItem;
+       const span = document.createElement("span");
+       const button = document.createElement("button");
+       const txt = document.createTextNode("Delete item");
+       span.className = "close";
+       button.className = "dynamic-buttons";
+       button.appendChild(txt);
+       span.appendChild(button);
+       myNodelist.appendChild(span);
+
+       // Creating a edit button
+       const span2 = document.createElement("span");
+       const button2 = document.createElement("button");
+       const txt2 = document.createTextNode("Modify task");
+       span2.className = "change";
+       button2.className = "dynamic-buttons";
+       button2.appendChild(txt2);
+       span2.appendChild(button2);
+       myNodelist.appendChild(span2);
+
+        return data;
+
         }
+        
         else
         {
             console.log('Error while posting todo with status : ${res.status}');
             return false;
         }
+        
     }
     catch(err)
     {
         console.error(err);
     }
+    
 }
+const disableButton = document.getElementById("add-item");
+disableButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    postTodo();
+});
+
+
+
 
 
